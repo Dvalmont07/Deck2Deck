593179299206c3da442dadeef4c64548e9b4eebe
Danilo Matos <dvalmont07@hotmail.com>
2021-08-16 14:48:05 -0300

    add & remove commands

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   normalDeck: Card[] = [];
   randomDeck: Card[] = [];
   selectedCard: Card = { suit: "", value: "" };
+  selectedIdex: any = { deck: "deckOne", index: 0 };
 
   ngOnInit() {
     this.dealRandomDeck();
@@ -27,7 +28,26 @@ export class AppComponent {
   dealNormalDeck() {
     this.normalDeck = this.deck.mountNormalDeck();
   }
-  addCard(deckTo: Card[], card: Card) {
-    this.deck.addCard(deckTo, card);
+  addCard(deckFrom: Card[], deckTo: Card[], card: Card) {
+
+    if (this.selectedCard.suit && this.selectedCard.value) {
+      if (this.deck.addCard({ myDeck: deckTo, card })) {
+        this.deck.removeCard(deckFrom, card);
+        console.log("this.selectedIdex.index 1", this.selectedIdex.index);
+        console.log("TF", this.selectedIdex.index == deckFrom.length);
+
+        if (this.selectedIdex.index == deckFrom.length) {
+          this.selectedCard = deckFrom[this.selectedIdex.index - 1];
+          this.selectedIdex.index--;// = { deck: this.selectedIdex.deck, index: this.selectedIdex.index - 1 }
+        } else {
+          this.selectedCard = deckFrom[this.selectedIdex.index];
+        }
+      }
+    }
+    console.log("deckFrom[this.selectedIdex.index]", deckFrom[this.selectedIdex.index]);
+    // console.log("deckFrom[deckFrom.length - 1]", deckFrom[deckFrom.length - 1]);
+    console.log("this.selectedCard", this.selectedCard);
+    console.log("ggg", deckFrom[this.selectedIdex.index] == deckFrom[deckFrom.length - 1]);
+
   }
 }
